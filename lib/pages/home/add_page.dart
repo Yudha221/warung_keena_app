f532f20e33aacddb94594ebaa1153edecf93ce60
Yudha221 <yudhaghilang2@gmail.com>
2025-02-10 13:16:22 +0700

    update

--- lib/pages/home/add_page.dart
+++ lib/pages/home/add_page.dart
@@ -8,6 +8,10 @@ class AddPage extends StatefulWidget {
 }
 
 class _AddPageState extends State<AddPage> {
+  final _formKey = GlobalKey<FormState>();
+  final titleController = TextEditingController();
+  final priceController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,6 +29,44 @@ class _AddPageState extends State<AddPage> {
                   fontWeight: FontWeight.bold),
             ),
           ),
+        ),
+      ),
+      body: Form(
+        key: _formKey,
+        child: ListView(
+          padding: const EdgeInsets.all(16),
+          children: [
+            TextFormField(
+              controller: titleController,
+              decoration: const InputDecoration(
+                  labelText: 'Menu', border: OutlineInputBorder()),
+              validator: (value) =>
+                  value == null || value.isEmpty ? 'Menu wajib diisi' : null,
+            ),
+            const SizedBox(height: 16),
+            TextFormField(
+              controller: priceController,
+              decoration: const InputDecoration(
+                  labelText: 'Harga', border: OutlineInputBorder()),
+              keyboardType: TextInputType.number,
+              validator: (value) =>
+                  value == null || value.isEmpty ? 'Harga wajib diisi' : null,
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  double price = double.tryParse(priceController.text) ?? 0.0;
+
+                  // Tambahkan proses yang ingin dilakukan dengan price (misalnya print atau simpan ke database)
+                  print("Judul: ${titleController.text}, Price: $price");
+
+                  Navigator.pop(context);
+                }
+              },
+              child: const Text('Simpan'),
+            ),
+          ],
         ),
       ),
     );
